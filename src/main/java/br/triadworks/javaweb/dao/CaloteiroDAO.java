7c74f6c3ec963504d64b25fa0ff920127c5ee92d
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-04-10 17:14:46 -0300

    Create Servlet for Include Caloteiro from form page and packages DAO, Model with Bean and DAO

---
+++ src/main/java/br/triadworks/javaweb/dao/CaloteiroDAO.java
@@ -0,0 +1,158 @@
+package br.triadworks.javaweb.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import br.triadworks.javaweb.model.Caloteiro;
+
+public class CaloteiroDAO {
+
+	private Connection connection;
+
+
+	public CaloteiroDAO() {
+		this.connection = new ConnectionFactory().getConnection();
+	}
+
+
+	public void includeCaloteiro(Caloteiro caloteiro) {
+		String stmt = "insert into caloteiro " +
+				"(name, email, debt, debtDate) " +
+				"values (?,?,?,?)";
+		try {
+			PreparedStatement pstmt = this.connection.prepareStatement(stmt);
+
+			pstmt.setString(1, caloteiro.getName());
+			pstmt.setString(2, caloteiro.getEmail());
+			pstmt.setInt(3, caloteiro.getDebt());
+			pstmt.setDate(4, new Date(caloteiro.getDebtDate().getTimeInMillis()));
+
+			pstmt.execute();
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public List<Caloteiro> getCaloteiroList() {
+		try {
+			PreparedStatement stmt = this.connection.
+					prepareStatement("select * from caloteiro;");
+
+			List<Caloteiro> caloteiros = new ArrayList<>();
+			Caloteiro caloteiro = null;
+			ResultSet rs = stmt.executeQuery();
+
+			while(rs.next()) {
+				Long idCaloteiro = (long) rs.getInt("id");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				int debt = rs.getInt("debt");
+				Calendar debtDate = Calendar.getInstance();
+				debtDate.setTime(rs.getDate("debtDate"));
+
+				caloteiro = new Caloteiro();
+
+				caloteiro.setId(idCaloteiro);
+				caloteiro.setName(name);
+				caloteiro.setEmail(email);
+				caloteiro.setDebt(new Integer(debt));
+				caloteiro.setDebtDate(debtDate);
+
+				caloteiros.add(caloteiro);
+			}
+
+			rs.close();
+			stmt.close();
+			return caloteiros;
+
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+
+	public void updateCaloteiro(Caloteiro caloteiro) {
+		String sql = "update caloteiro set name=?, email=?,"+
+				"debt=?, debtDate=? where id=?";
+
+		try {
+			PreparedStatement stmt = this.connection.prepareStatement(sql);
+
+			stmt.setString(1, caloteiro.getName());
+			stmt.setString(2, caloteiro.getEmail());
+			stmt.setInt(3, caloteiro.getDebt());
+			stmt.setDate(4, new Date(caloteiro.getDebtDate().getTimeInMillis()));
+			stmt.setLong(5, caloteiro.getId());
+
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+
+	public void deleteCaloteiro(Caloteiro caloteiro) {
+		String sql = "delete from caloteiro where id=?";
+
+		try {
+			PreparedStatement stmt = this.connection.prepareStatement(sql);
+
+			stmt.setLong(1, caloteiro.getId());
+
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+
+	public Caloteiro getCaloteiroByID(Long id) {
+		try {
+			PreparedStatement stmt = this.connection.
+					prepareStatement("select * from caloteiro where id=?");
+			stmt.setLong(1, id);
+
+			Caloteiro caloteiro = null;
+			ResultSet rs = stmt.executeQuery();
+
+			if(rs.next()) {
+				Long idCaloteiro = rs.getLong("id");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				int debt = rs.getInt("debt");
+				Calendar debtDate = Calendar.getInstance();
+				debtDate.setTime(rs.getDate("debtDate"));
+
+				caloteiro = new Caloteiro();
+
+				caloteiro.setId(idCaloteiro);
+				caloteiro.setName(name);
+				caloteiro.setEmail(email);
+				caloteiro.setDebt(new Integer(debt));
+				caloteiro.setDebtDate(debtDate);
+			}
+
+			rs.close();
+			stmt.close();
+			return caloteiro;
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
+
+
+
+
---
+++ src/main/java/br/triadworks/javaweb/servlets/IncludeCaloteiroServlet.java
@@ -0,0 +1,56 @@
+package br.triadworks.javaweb.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import br.triadworks.javaweb.dao.CaloteiroDAO;
+import br.triadworks.javaweb.model.Caloteiro;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/adicionaCaloteiro")
+public class IncludeCaloteiroServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String debt = request.getParameter("debt");
+		String debtDate = request.getParameter("debtDate");
+		Calendar debtDateParsed = null;
+		try {
+			Date debtDateFormatted = new SimpleDateFormat("dd/MM/yyyy").parse(debtDate);
+			debtDateParsed = Calendar.getInstance();
+			debtDateParsed.setTime(debtDateFormatted);
+		}catch (ParseException e) {
+			out.println("Erro de conversão de data");
+		}
+		
+		Caloteiro caloteiro = new Caloteiro();
+		caloteiro.setName(name);
+		caloteiro.setEmail(email);
+		caloteiro.setDebt(new Integer(debt));
+		caloteiro.setDebtDate(debtDateParsed);
+	
+		CaloteiroDAO dao = new CaloteiroDAO();
+		dao.includeCaloteiro(caloteiro);
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Caloteiro " + caloteiro.getName() + " adicionado com sucesso");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+	
+}
